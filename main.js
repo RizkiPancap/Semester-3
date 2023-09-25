03dbd541a6214628f487ba82b0a823621ce5ff73
RizkiPancap <120879907+RizkiPancap@users.noreply.github.com>
2023-09-26 00:49:52 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+let nama = "Rizki Panca Pamungkas";
+let nim = "20220040022";
+
+console.log("Nama: " + nama);
+console.log("NIM: " + nim);
+
+let angka1 = 16;
+let angka2 = 21;
+
+let jumlah = angka1 + angka2;
+console.log("Hasil penjumlahan: " + jumlah);
