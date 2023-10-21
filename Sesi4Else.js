5a7fe9176c5c973eea98c4b75f1d468a50aa6928
RizkiPancap <120879907+RizkiPancap@users.noreply.github.com>
2023-10-22 02:09:40 +0700

    Add files via upload

---
+++ Sesi4Else.js
@@ -0,0 +1,18 @@
+let pendapatan = 7000000; // Dapat Di Rubah jumlah pendapatan sesuai kebutuhan
+
+if (pendapatan <= 200000) {
+  let uangJasa = 10000;
+  let komisi = pendapatan * 0.1;
+  let totalKomisi = uangJasa + komisi;
+  console.log("Total komisi yang didapatkan: Rp." + totalKomisi);
+} else if (pendapatan > 200000 && pendapatan <= 500000) {
+  let uangJasa = 20000;
+  let komisi = pendapatan * 0.15;
+  let totalKomisi = uangJasa + komisi;
+  console.log("Total komisi yang didapatkan: Rp." + totalKomisi);
+} else if (pendapatan > 500000) {
+  let uangJasa = 30000;
+  let komisi = pendapatan * 0.2;
+  let totalKomisi = uangJasa + komisi;
+  console.log("Total komisi yang didapatkan: Rp." + totalKomisi);
+}
